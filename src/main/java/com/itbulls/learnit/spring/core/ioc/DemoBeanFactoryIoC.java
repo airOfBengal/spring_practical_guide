b0f8c5ff4f1001a8dbf6566dc3aa29663d6d2987
Md. Atiqul Islam <air.cse.pbn@gmail.com>
2024-10-19 11:27:12 +0600

    initial commit

---
+++ src/main/java/com/itbulls/learnit/spring/core/ioc/DemoBeanFactoryIoC.java
@@ -0,0 +1,24 @@
+package com.itbulls.learnit.spring.core.ioc;
+
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+public class DemoBeanFactoryIoC {
+    public static void main(String[] args) {
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+        Resource resource = new ClassPathResource("applicationContext.xml");
+        beanDefinitionReader.loadBeanDefinitions(resource);
+
+        User user1 = beanFactory.getBean("userBean", User.class);
+        System.out.println(user1);
+
+        User user2 = beanFactory.getBean("user", User.class);
+        System.out.println(user2);
+
+        System.out.println("user1 == user2: "+ (user1 == user2));
+    }
+}
